6025b0df47d27b469fb7086282d1f8d93f48c646
admin <daominhhiep97@gmail.com>
2021-07-14 22:41:02 +0700

    rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,10 +1,71 @@
 import React from 'react'
 import "./rightbar.css"
+import {Cake} from "@material-ui/icons";
 
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            Rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <Cake htmlColor={"tomato"} className="birthdayImg"/>
+                    <span className="birthdayText">
+                        <b>Hiep</b> and <b>3 other friends</b> have birthday today
+                    </span>
+                </div>
+                <img src={"assets/ads.jpg"} alt="" className="rightbarAd"/>
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src={"assets/avatar.jpg"} alt="" className="rightbarProfileImg"/>
+                            <span className="rightbarOnline"/>
+                        </div>
+                        <span className="rightbarUsername">Hiep</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src={"assets/avatar.jpg"} alt="" className="rightbarProfileImg"/>
+                            <span className="rightbarOnline"/>
+                        </div>
+                        <span className="rightbarUsername">Hiep</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src={"assets/avatar.jpg"} alt="" className="rightbarProfileImg"/>
+                            <span className="rightbarOnline"/>
+                        </div>
+                        <span className="rightbarUsername">Hiep</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src={"assets/avatar.jpg"} alt="" className="rightbarProfileImg"/>
+                            <span className="rightbarOnline"/>
+                        </div>
+                        <span className="rightbarUsername">Hiep</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src={"assets/avatar.jpg"} alt="" className="rightbarProfileImg"/>
+                            <span className="rightbarOnline"/>
+                        </div>
+                        <span className="rightbarUsername">Hiep</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src={"assets/avatar.jpg"} alt="" className="rightbarProfileImg"/>
+                            <span className="rightbarOnline"/>
+                        </div>
+                        <span className="rightbarUsername">Hiep</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src={"assets/avatar.jpg"} alt="" className="rightbarProfileImg"/>
+                            <span className="rightbarOnline"/>
+                        </div>
+                        <span className="rightbarUsername">Hiep</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
