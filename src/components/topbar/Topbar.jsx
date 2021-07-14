633fd44dc6c1ffabd556510e84b0d333025fbf44
Hiep Dao Minh <hiep_dm@septeni-technology.jp>
2021-07-14 14:24:27 +0700

    Body component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,42 +1,10 @@
 import React from 'react'
 import "./topbar.css"
-import {Chat, Notifications, Person, Search} from "@material-ui/icons";
 
 export default function Topbar() {
     return (
-        <div className="topbarContainer">
-            <div className="topbarLeft">
-                <span className="logo">Social</span>
-            </div>
-            <div className="topbarCenter">
-                <div className="searchBar">
-                    <Search className="searchIcon"/>
-                    <input type="text"
-                           className="searchInput"
-                           placeholder="Search for friend, post or video"/>
-                </div>
-            </div>
-            <div className="topbarRight">
-                <div className="topbarLinks">
-                    <div className="topbarLink">Homepage</div>
-                    <div className="topbarLink">Timeline</div>
-                </div>
-                <div className="topbarIcons">
-                    <div className="topbarIconItem">
-                        <Person/>
-                        <span className="topbarIconBadge">1</span>
-                    </div>
-                    <div className="topbarIconItem">
-                        <Chat/>
-                        <span className="topbarIconBadge">2</span>
-                    </div>
-                    <div className="topbarIconItem">
-                        <Notifications/>
-                        <span className="topbarIconBadge">3</span>
-                    </div>
-                </div>
-                <img src="../../../public/assets/avatar.jpg" alt="" className="topbarImg"/>
-            </div>
+        <div>
+            Topbar
         </div>
     )
 }
