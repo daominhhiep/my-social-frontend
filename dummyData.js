ff0f55f317651cd878c5f40042a6c9069f9085cb
Hiep Dao Minh <hiep_dm@septeni-technology.jp>
2021-07-15 12:56:00 +0700

    data

---
+++ dummyData.js
@@ -0,0 +1,75 @@
+export const Users = [
+    {
+        id: 1,
+        profilePicture: "assets/avatar.jpg",
+        username: "Hiep",
+    },
+    {
+        id: 2,
+        profilePicture: "assets/avatar.jpg",
+        username: "Minh Hiep",
+    },
+    {
+        id: 3,
+        profilePicture: "assets/avatar.jpg",
+        username: "Peter",
+    },
+    {
+        id: 4,
+        profilePicture: "assets/avatar.jpg",
+        username: "Lmao",
+    },
+    {
+        id: 5,
+        profilePicture: "assets/avatar.jpg",
+        username: "Ben",
+    },
+    {
+        id: 6,
+        profilePicture: "assets/avatar.jpg",
+        username: "Biden",
+    },
+    {
+        id: 7,
+        profilePicture: "assets/avatar.jpg",
+        username: "Okla",
+    },
+    {
+        id: 8,
+        profilePicture: "assets/avatar.jpg",
+        username: "John",
+    },
+]
+
+export const Posts = [
+    {
+        id: 1,
+        desc: "Hello",
+        photo: "assets/post.jpeg",
+        date: "5 mins ago",
+        userId: 1,
+        like: 32,
+        comment: 9,
+        share: 10,
+    },
+    {
+        id: 2,
+        desc: "Ok",
+        photo: "assets/avatar.jpg",
+        date: "5 mins ago",
+        userId: 2,
+        like: 123,
+        comment: 12,
+        share: 15,
+    },
+    {
+        id: 3,
+        desc: "Hihi",
+        photo: "assets/post.jpeg",
+        date: "5 mins ago",
+        userId: 5,
+        like: 88,
+        comment: 54,
+        share: 35,
+    },
+]
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import "./post.css"
 import {MoreVert, Public} from "@material-ui/icons";
 
-export default function Post() {
+export default function Post(props) {
+    const { post } = props
     return (
         <div className="post">
             <div className="postWrapper">
@@ -10,7 +11,7 @@ export default function Post() {
                     <div className="postTopLeft">
                         <img src={"assets/avatar.jpg"} alt="" className="postProfileImg"/>
                         <span className="postUsername">Hiep</span>
-                        <span className="postDate">5 min ago</span>
+                        <span className="postDate">{post.date}</span>
                         <Public className="postPrivacy"/>
                     </div>
                     <div className="postTopRight">
@@ -18,18 +19,18 @@ export default function Post() {
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">Hello</span>
-                    <img src={"assets/post.jpeg"} alt="" className="postImg"/>
+                    <span className="postText">{post.desc}</span>
+                    <img src={post.photo} alt="" className="postImg"/>
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <img src={"assets/like.png"} alt="" className="icon"/>
                         <img src={"assets/heart.png"} alt="" className="icon"/>
-                        <span className="postLikeCounter">32 like</span>
+                        <span className="postLikeCounter">{post.like} like</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">90 Comment</span>
-                        <span className="postCommentText">10 Share</span>
+                        <span className="postCommentText">{post.comment} Comment</span>
+                        <span className="postCommentText">{post.share} Share</span>
                     </div>
                 </div>
             </div>
