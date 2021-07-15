32afe704cdeb1c1f4c8303ce5461a9823eb7f7a2
Hiep Dao Minh <hiep_dm@septeni-technology.jp>
2021-07-15 14:49:01 +0700

    profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
-      <Home/>
+        <Profile/>
   )
 }
 export default App
